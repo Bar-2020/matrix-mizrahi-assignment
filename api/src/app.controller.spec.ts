5bca6c0a2fb45498a8d0fd4ffd8fd66edb76a0b5
Bar Almog <baralmog@protonmail.com>
2024-10-13 23:28:37 +0300

    Modified app test to the minimum

--- api/src/app.controller.spec.ts
+++ api/src/app.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { CalculatorService } from './calculator/calculator.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,15 +10,13 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, AuthService, CalculatorService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
+  it('should be defined', () => {
+    expect(appController).toBeDefined();
   });
 });
