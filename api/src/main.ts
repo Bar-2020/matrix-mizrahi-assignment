e95940b27889d14ff97fb1b827dbc02a9d24a0a3
Bar Almog <baralmog@protonmail.com>
2024-10-13 15:43:48 +0300

    first commit

---
+++ api/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Calculator API')
+    .setDescription('An API to perform basic arithmetic operations.')
+    .setVersion('1.0')
+    .addTag('calculator')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
