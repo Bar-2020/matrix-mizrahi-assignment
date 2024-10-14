69087f9b6096ee569d0d0d8f2d2cf34ca1de0c28
Bar Almog <baralmog@protonmail.com>
2024-10-14 14:36:53 +0300

    Added documentation

--- api/src/auth/constants.ts
+++ api/src/auth/constants.ts
@@ -1,3 +1,4 @@
+// Contains the secret used to sign JWTs. On production apps this would be in dotenv or a secret management service.
 export const jwtConstants = {
   secret:
     'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
